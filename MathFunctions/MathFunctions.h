9f2a5c2ceb2669aca08429344d383d35963a2d9d
Maks-IT <maksym.sadovnychyy@gmail.com>
2019-03-08 10:33:57 +0100

    Init

---
+++ MathFunctions/MathFunctions.h
@@ -0,0 +1,7 @@
+
+/* sqrt example */
+
+#include <stdio.h>      /* printf */
+#include <math.h>       /* sqrt */
+
+double mysqrt(double value); //Function prototype
---
+++ MathFunctions/mysqrt.cpp
@@ -0,0 +1,17 @@
+﻿
+#include "MathFunctions.h"
+#include "TutorialConfig.h"
+
+double mysqrt(double value)
+{
+	double result;
+
+	// if we have both log and exp then use them
+	#if defined (HAVE_LOG) && defined (HAVE_EXP)
+		result = exp(log(value)*0.5);
+	#else // otherwise use an iterative approach
+		result = sqrt(value);
+	#endif;
+
+	return result;
+}
